c00ee8f4cd6651b52cf0006daf58622eae88a551
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-06-05 14:59:26 -0500

    Changed libs version

---
+++ Calm/Views/Firebase/CustomLoginView.swift
@@ -0,0 +1,66 @@
+//
+//  CustomLoginView.swift
+//  Calm
+//
+//  Created by Aarón Cervantes Álvarez on 05/06/21.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseUI
+
+struct CustomLoginView : UIViewControllerRepresentable {
+    
+    var dismiss : (_ error : Error? ) -> Void
+    
+    func makeCoordinator() -> CustomLoginView.Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIViewController
+    {
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        let providers : [FUIAuthProvider] = [
+            FUIEmailAuth(),
+            FUIGoogleAuth.init(authUI: authUI!),
+            FUIOAuth.twitterAuthProvider(),
+            FUIOAuth.appleAuthProvider(),
+            FUIPhoneAuth.init(authUI: authUI!)
+        ]
+
+        authUI?.providers = providers
+        authUI?.delegate = context.coordinator
+        
+        let authViewController = authUI?.authViewController()
+
+        return authViewController!
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<CustomLoginView>)
+    {
+        
+    }
+    
+    //coordinator
+    class Coordinator : NSObject, FUIAuthDelegate {
+        var parent : CustomLoginView
+        
+        init(_ customLoginViewController : CustomLoginView) {
+            self.parent = customLoginViewController
+        }
+        
+        // MARK: FUIAuthDelegate
+        func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+            if let error = error {
+                parent.dismiss(error)
+            }
+            else {
+                parent.dismiss(nil)
+            }
+        }
+        
+        func authUI(_ authUI: FUIAuth, didFinish operation: FUIAccountSettingsOperationType, error: Error?) {
+        }
+    }
+}
--- Podfile
+++ Podfile
@@ -1,17 +1,15 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '10.0'
+platform :ios, '14.0'
 
 target 'Calm' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Calm
-  pod 'Firebase/Analytics'
-  pod 'Firebase/Auth'
-  pod 'Firebase/Firestore'
-  pod 'Firebase/Storage'
-  
-  pod 'FirebaseFirestoreSwift'
-
-  pod 'FirebaseUI'
+pod 'FirebaseCore', '~> 7.2'
+pod 'FirebaseAuth', '~> 7.2'
+pod 'FirebaseStorage', '~> 7.2'
+pod 'FirebaseUI', '~> 10.0.2'
+pod 'FirebaseFirestore', '~> 7.2'
+pod 'FirebaseFirestoreSwift', '= 7.9.0-beta' 
 end
