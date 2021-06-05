c106c09a038df44587f213e7b9c8298aa57df089
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-06-05 14:23:34 -0500

    Added init config

---
+++ Calm/Views/Firebase/FirebaseUIView.swift
@@ -0,0 +1,24 @@
+//
+//  FirebaseUIView.swift
+//  Calm
+//
+//  Created by Aarón Cervantes Álvarez on 05/06/21.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseUI
+
+struct FirebaseUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+public var screenWidth: CGFloat {
+    return UIScreen.main.bounds.width
+}
+
+public var screenHeight: CGFloat {
+    return UIScreen.main.bounds.height
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Calm' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Calm
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Storage'
+  
+  pod 'FirebaseFirestoreSwift'
+
+  pod 'FirebaseUI'
+end
